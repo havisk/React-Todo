664112e172f99b0ad9984d22e34cc87c8ef1bddc
Kool Havis <havisk@gmail.com>
2017-08-05 23:19:47 -0400

    add todos reducer and test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -27,4 +27,36 @@ describe('Reducers', () => {
           expect(res).toEqual(true);
       });
    });
+
+   describe('todosReducer', () => {
+       it('should add new todo', () => {
+           let action = {
+               type: 'ADD_TODO',
+               text: 'walk home'
+           };
+           let res = reducers.todosReducer(df([]), df(action));
+
+           expect(res.length).toEqual(1);
+           expect(res[0].text).toEqual(action.text)
+       });
+       it('should toggle todo', () => {
+          let todos = [{
+              id: '1',
+              text: 'hello',
+              completed: true,
+              createdAt: 234,
+              completedAt: 768
+          }];
+
+          let action = {
+              type: 'TOGGLE_TODO',
+              id: '1'
+          };
+
+          let res = reducers.todosReducer(df(todos), df(action));
+
+          expect(res[0].completedAt).toEqual(undefined);
+          expect(res[0].completed).toEqual(false);
+       });
+   });
 });
